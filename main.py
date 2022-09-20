c45bd6a5bf723b150e9b83b5db22938c741d2eb1
jo181 <jo181@student.le.ac.uk>
2022-09-20 13:51:53 +0100

    Added Todo List

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # TODO LIST
 # 1. Create a github account.
 # 2. Download Pycharm.
-# 3. Look into Tkinter (framework we are using) https://docs.python.org/3/library/tkinter.html
+# 3. Look into Tkinter (framework we are using)  https://docs.python.org/3/library/tkinter.html
 
