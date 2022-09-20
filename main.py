71cdb6cbff40a387061b836ce4bbbcbb8b708570
jo181 <jo181@student.le.ac.uk>
2022-09-20 14:13:42 +0100

    added more to the todo list

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # TODO LIST
-# 1. Create a github account.
-# 2. Download Pycharm.
-# 3. Look into Tkinter (framework we are using)  https://docs.python.org/3/library/tkinter.html
+# 1. Create a github account. (done)
+# 2. Download git https://git-scm.com/download/win
+# 3. Make sure to create a file where the files for the project will kept.
+# 4. clone the repo " git clone https://github.com/Jomoregie1/Revision_app.git " -- needs to be done on the terminal.
+# 5. Download Pycharm.
+# 6. Look into Tkinter (framework we are using)  https://docs.python.org/3/library/tkinter.html
 
